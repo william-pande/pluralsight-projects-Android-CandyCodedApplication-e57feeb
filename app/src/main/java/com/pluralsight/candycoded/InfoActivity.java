91e9bdfe78b2aacd47429e62104f69445909ae91
WilliamPande <pandewilliam100@gmail.com>
2020-10-15 08:31:35 +0300

    Finalised step 4

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,7 +34,10 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Intent intentDial = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        intentDial.setData(uri);
+        startActivity(intentDial);
+    }
 }
